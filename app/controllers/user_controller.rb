2f17269f2f70473927e66ab774c8cc9fd671d59f
Nicolas Alkhoury <nicolaskhoury98@gmail.com>
2022-01-11 06:20:01 -0500

    Configure the routes and functional controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,5 +4,19 @@ class UserController < ApplicationController
   end
 
   def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to '/', notice: 'Thank you for signing up!'
+    else
+      render '/signup'
+    end
   end
+
+end
+
+private
+
+def user_params
+  params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
 end
