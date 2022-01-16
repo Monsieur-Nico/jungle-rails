edd816d4a73e9c3d6266f6947244b0d3754a75bf
Nicolas Alkhoury <nicolaskhoury98@gmail.com>
2022-01-16 14:09:03 -0500

    Fix creating a user problem

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -7,16 +7,15 @@ class UserController < ApplicationController
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
-      redirect_to '/', notice: 'Thank you for signing up!'
+      redirect_to '/'
     else
-      redirect_to '/user/new'
+      redirect_to '/signup'
     end
   end
-
 end
 
-private
+private 
 
 def user_params
-  params.require(:user).permit(:first_name, :last_name, :email, :password, presence: true, :password_confirmation)
+  params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,16 @@
 class User < ActiveRecord::Base
   has_secure_password
-
+  
   validates :first_name, presence: true 
   validates :last_name, presence: true
-  validates :email, uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 3 }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, confirmation: { case_sensitive: true }, length: {minimum: 5}
   validates :password_confirmation, presence: true
 
   def self.authenticate_with_credentials(email, password)
-   user = self.find_by_email(email.strip.downcase)
+    user = self.find_by_email(email.strip.downcase)
     if user && user.authenticate(password)
       user
-    else
-      nil
     end
   end
 end
